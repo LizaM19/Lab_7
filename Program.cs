3855453e91ba10a780dbdb9cd156eb5f9330f241
LizaM <65567063+LizaM19@users.noreply.github.com>
2020-11-27 13:01:39 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,7 +12,6 @@ namespace Program
             queue.Enqueue(v); //Помещаем v в очередь
 
             DIST[v] = 0;
-
            
             Console.Write("Результат обхода:  ");
             while (queue.Count != 0)
@@ -28,70 +27,73 @@ namespace Program
                     }
                 }
             }
-            Console.Write("      ");
-            
+            Console.Write("      ");   
         }
 
         static int Main(string[] args)
         {
-
             Console.Write("Введите размерность матрицы:");  // 1 2  не взвешенный направленный
-            int size = Convert.ToInt32(Console.ReadLine()); // 1 1  не взвешенный не направленный
+            int size=3;// = Convert.ToInt32(Console.ReadLine()); // 1 1  не взвешенный не направленный
+
+            for (int i=0; i< args.Length; i++) {
+                if (args[i] == "size")
+                {
+                    size = int.Parse(args[i + 1]);
+                }
+            }
+
+
             int[,] M = new int[size, size];                 // 2 1  взвешенный не направленный
             int[] DIST = new int[size];                     // 2 2  взвешенный  направленный
-
-
             Random random = new Random();
 
             if (args.Length == 0)
             {
-                System.Console.WriteLine("Please enter a numeric argument.");
-                return 1;
-            }
-
-            int vzv = int.Parse(args[0]);
-            int napr = int.Parse(args[1]);
-            
-            if (vzv == 2)
-            {
-                for (int i = 0; i < size; i++)
-                {
-                    for (int j = 0; j < size; j++)
+                    for (int i = 0; i < size; i++)
                     {
-                        M[i, j] = random.Next(10);
-                        M[i, i] = 0;
+                        for (int j = 0; j < size; j++)
+                        {
+                            M[i, j] = random.Next(2);
+                            M[i, i] = 0;
+                        }
                     }
-                }
             }
 
-            else {
-                for (int i = 0; i < size; i++)
+            if (args.Length != 0)
+            {      
+                for (int k = 0; k < args.Length; k++)
                 {
-                    for (int j = 0; j < size; j++)
+                    if (args[k] == "v")
                     {
-                        M[i, j] = random.Next(2);
-                        M[i, i] = 0;
+                        for (int i = 0; i < size; i++)
+                        {
+                            for (int j = 0; j < size; j++)
+                            {
+                                M[i, j] = random.Next(10);
+                                M[i, i] = 0;
+                            }
+                        }
+                    }
+  
+
+                    if (args[k] == "n")
+                    {
+                        for (int i = 1; i < size; i++)
+                        {
+                            for (int j = 0; j < i; j++)
+                            {
+                                M[j, i] = M[i, j];
+
+                            }
+                        }
                     }
                 }
+
             }
 
-            if (napr == 1)
-            {
-                for (int i = 1; i < size; i++)
-                {
-                    for (int j = 0; j < i; j++)
-                    {     
-                        M[j, i] = M[i, j];
-                      
-                    }
-                }
-                
-            }
-
-                Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine("Сгененрированная матрица:\t");
-
-           
+ 
 
             for (int i = 0; i < size; i++)
             {
@@ -105,17 +107,14 @@ namespace Program
 
             for (int i = 0; i < size; i++)
             {
-
                 for (int j = 0; j < size; j++)
                 {
                     DIST[j] = -1;
                 }
 
                 BFSD(i, M, DIST, size);
-
-                
+ 
                 Console.Write("Расстояния от вершины  "+i+":  ");
-
                 for (int t = 0; t < size; t++)
                 {
                     Console.Write(DIST[t]+" ");
